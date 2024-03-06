1cb9a350eba98032498251808f62583d79dbed05
dslavicek <slavicek.david29@gmail.com>
2024-03-06 22:29:59 +0100

    implement movement

--- src/app.js
+++ src/app.js
@@ -52,10 +52,14 @@ const mapDisplay = document.querySelector('#map')
 function createMap(){
     for (let row = 0; row < mapHeight; row++){
         const mapRow = document.createElement('div');
+        //mapRow.setAttribute('row-id', row)
         mapDisplay.appendChild(mapRow);
         for(let col = 0; col < mapWidth; col++){
             const mapTile = document.createElement('img');
             mapTile.setAttribute('src', '../img/'+mapTiles[row*mapWidth+col].img);
+            mapTile.setAttribute('src-orig', '../img/'+mapTiles[row*mapWidth+col].img);
+            mapTile.setAttribute('row-id', row)
+            mapTile.setAttribute('col-id', col)
             console.log(mapTile, row, col);
             //mapDisplay.appendChild(mapTile);
             mapRow.appendChild(mapTile);
@@ -64,3 +68,107 @@ function createMap(){
 }
 
 createMap();
+
+var PlayerX = 0;
+var PlayerY = 0;
+const playerImPath = "../img/knight.png";
+
+var playerTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`)
+playerTile.setAttribute('src', playerImPath);
+
+
+document.addEventListener("keydown", function(event) {
+if (event.key === "d") {
+    // Function to be executed on "d" key press
+    movePlayerRight(PlayerX, PlayerY);
+}
+});
+
+document.addEventListener("keydown", function(event) {
+if (event.key === "a") {
+    // Function to be executed on "d" key press
+    movePlayerLeft(PlayerX, PlayerY);
+}
+});
+
+document.addEventListener("keydown", function(event) {
+if (event.key === "w") {
+    // Function to be executed on "d" key press
+    movePlayerUp(PlayerX, PlayerY);
+}
+});
+
+document.addEventListener("keydown", function(event) {
+if (event.key === "s") {
+    // Function to be executed on "d" key press
+    movePlayerDown(PlayerX, PlayerY);
+}
+});
+
+function movePlayerUp(row, col) {
+// Check if the move is valid (within map bounds and not a wall tile)
+if (PlayerY > 0) {    
+
+    // Update the position of the player tile visually (replace with your implementation)
+    const currentTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+    currentTile.setAttribute('src', currentTile.getAttribute('src-orig'))
+
+    PlayerY--;
+
+    const newTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+    newTile.setAttribute('src', playerImPath);
+} else {
+    console.log("Invalid move: cannot move up.");
+}
+}
+
+function movePlayerDown(row, col) {
+// Check if the move is valid (within map bounds and not a wall tile)
+if (PlayerY < mapHeight - 1) {    
+
+    // Update the position of the player tile visually (replace with your implementation)
+    const currentTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+    currentTile.setAttribute('src', currentTile.getAttribute('src-orig'))
+
+    PlayerY++;
+
+    const newTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+    newTile.setAttribute('src', playerImPath);
+} else {
+    console.log("Invalid move: cannot move down.");
+}
+}
+
+function movePlayerRight(row, col) {
+    // Check if the move is valid (within map bounds and not a wall tile)
+    if (PlayerX < mapWidth - 1) {    
+    
+        // Update the position of the player tile visually (replace with your implementation)
+        const currentTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+        currentTile.setAttribute('src', currentTile.getAttribute('src-orig'))
+    
+        PlayerX++;
+    
+        const newTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+        newTile.setAttribute('src', playerImPath);
+    } else {
+        console.log("Invalid move: cannot move right.");
+    }
+    }
+    
+    function movePlayerLeft(row, col) {
+    // Check if the move is valid (within map bounds and not a wall tile)
+    if (PlayerX > 0) {    
+    
+        // Update the position of the player tile visually (replace with your implementation)
+        const currentTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+        currentTile.setAttribute('src', currentTile.getAttribute('src-orig'))
+    
+        PlayerX--;
+    
+        const newTile = document.querySelector(`#map > div:nth-child(${PlayerY + 1}) > img[col-id="${PlayerX}"]`);
+        newTile.setAttribute('src', playerImPath);
+    } else {
+        console.log("Invalid move: cannot move left.");
+    }
+    }
